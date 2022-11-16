4ca75a78dce23a5df4e39b22a474304d1cc58d95
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2022-11-16 10:34:23 -0800

    Create dock_func.sh

---
+++ bash/dock_func.sh
@@ -0,0 +1,22 @@
+#remove container
+docker rm --force fce626371825aae3ace5431bf8ad4524ff4c0906f2fbffb61072b842eecf2e74
+
+#remove pod
+# Delete pods
+delete_pods() {
+  source ./hack/fornax_curl.zshrc
+
+  pod_ids=`sudo crictl pods | awk '{print $1}'`
+
+  for pod_id in $pod_ids
+  do
+    if [ $pod_id == "POD" ]; then
+      continue
+    fi
+    
+    echo $pod_id
+    sudo crictl stopp $pod_id
+    sleep 1
+    sudo crictl rmp $pod_id
+  done
+}
