0b44b6416d8575ea31a48120b9ba030c5048a40c
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2022-11-18 12:48:46 -0800

    Update debug.sh

--- bash/debug.sh
+++ bash/debug.sh
@@ -1,2 +1,6 @@
  tail -f nodeagent.logs
  
+ grep -i 'pod state' fornaxcore.logs | grep 'pod phase' | grep Fail | wc -l
+ 
+  grep -i 'pod state' fornaxcore.logs | grep 'pod phase' | grep Running | wc -l
+ 
