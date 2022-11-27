443f7a9ab57a8d37edc880bbebcede99e46408c4
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2022-11-26 21:52:45 -0800

    Create install_ubuntu_in_window.sh

---
+++ bash/install_ubuntu_in_window.sh
@@ -0,0 +1,31 @@
+https://ubuntu.com/tutorials/install-ubuntu-on-wsl2-on-windows-10#3-download-ubuntu
+
+https://learn.microsoft.com/en-us/windows/wsl/install
+
+wsl --list --online
+
+wsl --install -d <DistroName>
+
+wsl --install -d Ubuntu-20.04
+
+Ubuntu-20.04    Ubuntu 20.04 LTS
+
+Install WSL 2 on Windows 10
+First we need to enable WSL and Virtualization on the machine by running the following commands:
+
+	dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+	dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+	Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+
+
+
+Then, reboot your machine.
+
+Download the kernel patch for WSL 2 by following the link here:  https://aka.ms/wsl2kernel, and get it installed.
+
+Finally, enable WSL 2 as default:
+
+	wsl --set-default-version 2
+
+Resources
+https://www.omgubuntu.co.uk/how-to-install-wsl2-on-windows-10
