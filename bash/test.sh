ec48c4d187285358e74200505d41d1b2ed21374c
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2022-11-28 15:56:30 -0800

    Update test.sh

--- bash/test.sh
+++ bash/test.sh
@@ -1,3 +1,14 @@
 https://kubernetes.io/docs/concepts/
 
 nohup ./bin/simulatenode --num-of-node 1000 > node.log 2
+
+DZ: Staff Software Engineer • 044033 Cloud Compute
+
+https://www.linkedin.com/in/shan-angela-gao/
+
+Angela Gao (Futurewei HR): gaoshan717@hotmail.com
+Senior HRBP
+028462 Campus Sourcing
+
+10900 NE 8th St.
+Bellevue Washington 98004
